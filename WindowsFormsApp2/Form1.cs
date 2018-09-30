6620899cd014211a37ec2d85f68bef99a4db3435
meletlamc <37398434+meletlamc@users.noreply.github.com>
2018-09-30 17:02:16 +0200

    Initial commit

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Notepad++\Notepad++.exe");
+            //System.Diagnostics.Process.Start("skype.exe");
+        }
+    }
+}
